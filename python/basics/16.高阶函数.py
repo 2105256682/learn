59401bec7c042001387cebc3c44b2f12cac37bc9
ye <2105256682@qq.com>
2026-06-08 10:40:26 +0800

    python笔记

--- python/basics/16.高阶函数.py
+++ python/basics/16.高阶函数.py
@@ -9,14 +9,3 @@ def generate_code(length=4):
     code = ''.join(random.choice(chars) for _ in range(length))
     return code
 
-# 登录验证功能
-def login():
-    real_code = generate_code(4)
-    print("验证码：", real_code)
-    user_input = input("请输入验证码：")
-    if user_input == real_code:
-        print("登录成功")
-    else:
-        print("验证码错误")
-
-login()
