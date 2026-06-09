f306630bad0c3fa82eec487036f13e9a19b2b459
ye <2105256682@qq.com>
2026-06-09 21:47:38 +0800

    python笔记

---
+++ python/basics/17.文件操作.py
@@ -0,0 +1,6 @@
+# wf = open('test.txt', 'r+')
+#
+# wf.seek(1, 0)
+# wf.write('hello')
+# wf.close()
+
---
+++ python/basics/18.文件与文件夹操作.py
@@ -0,0 +1,19 @@
+import os
+
+# 1. 查看当前目录
+print("当前目录：", os.getcwd())
+
+# 2. 列出文件
+print("目录文件：", os.listdir())
+
+# 3. 判断文件是否存在
+if os.path.exists("test.txt"):
+    print("test.txt 存在")
+    print("文件大小：", os.path.getsize("test.txt"))
+
+# 4. 重命名
+# os.rename("test.txt", "new.txt")
+
+# 5. 删除文件
+# os.remove("delete.txt")
+
