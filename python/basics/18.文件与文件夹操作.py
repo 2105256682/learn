c9125ce4540014af3df66055c6ed2bb242950a45
ye <2105256682@qq.com>
2026-06-10 21:30:03 +0800

    python笔记

--- python/basics/18.文件与文件夹操作.py
+++ python/basics/18.文件与文件夹操作.py
@@ -7,9 +7,6 @@ print("当前目录：", os.getcwd())
 print("目录文件：", os.listdir())
 
 # 3. 判断文件是否存在
-if os.path.exists("test.txt"):
-    print("test.txt 存在")
-    print("文件大小：", os.path.getsize("test.txt"))
 
 # 4. 重命名
 # os.rename("test.txt", "new.txt")
